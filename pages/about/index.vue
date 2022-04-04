e5b8176faa7a091b6dc8b2a7624e5ebd36b67c87
jmeteor <jerzy.zientek@gmail.com>
2022-04-04 20:59:19 +0200

    Add about static page

---
+++ pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <pre>{{ aboutData }}</pre>
+</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let aboutData;
+    try {
+      aboutData = await $content("site/about").fetch();
+    } catch (e) {
+      error({ message: "Project not found" });
+    }
+    return { aboutData };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
